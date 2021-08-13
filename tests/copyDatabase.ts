b59cda49acf146fbe17665747a2dc1d5b442df23
Yusuf Ali <ali@yusuf.email>
2021-08-13 10:16:05 -0400

    test: use pg_dump and and pg_restore for migrations

--- tests/copyDatabase.ts
+++ tests/copyDatabase.ts
@@ -1,56 +1,77 @@
+/* eslint-disable no-console */
 import fs from 'fs';
-import { exec } from 'child_process';
+import { spawn } from 'child_process';
 import url from 'url';
 
 const defaultDB = {
   port: 5432, hostname: '127.0.0.1', pathname: '/db', auth: 'postgres:postgres',
 };
+const pgDumpLocation = '/tmp/backup';
 
-const readURL = url.parse(process.env.DATABASE_READ_URL || '') || defaultDB;
-const dbURL = url.parse(process.env.DATABASE_URL || '') || defaultDB;
+const deleteBackup = () => {
+  if (fs.existsSync(pgDumpLocation)) {
+    fs.unlinkSync(pgDumpLocation);
+  }
+};
+
 const run = (
   cmd: string,
+  args: string[],
   env: { [key: string]: string },
-): Promise<string> => new Promise(
-  (resolve, reject) => exec(
-    cmd,
-    { env },
-    (error: Error | null, stdout: string) => {
-      if (error) {
-        return reject(error);
-      }
+): Promise<number | null> => new Promise(
+  (resolve, reject) => {
+    // @ts-ignore
+    console.log(cmd, args);
+    const child = spawn(cmd, args, { env });
 
-      return resolve(stdout);
-    },
-  ),
+    child.on('error', (e) => reject(e));
+    child.on('close', (code) => resolve(code));
+    child.on('exit', (code) => resolve(code));
+  },
 );
 
 const copyTables = async () => {
-  const fname = 'backup';
-  const dump: string = await run(
-    'pg_dump',
-    {
-      PGPORT: (readURL.port || 1).toString(),
-      PGHOST: readURL.hostname || '',
-      PGDATABASE: (readURL.pathname || '').replace('/', ''),
-      PGUSER: (readURL.auth || defaultDB.auth).split(':')[0],
-      PGPASSWORD: (readURL.auth || defaultDB.auth).split(':')[1],
-    },
-  );
-  fs.writeFileSync(fname, dump);
+  deleteBackup();
+  const readURL = url.parse(process.env.DATABASE_READ_URL || '') || defaultDB;
+  const dbURL = url.parse(process.env.DATABASE_URL || '') || defaultDB;
 
-  await run(
-    `psql -f ${fname}`,
-    {
-      PGPORT: (dbURL.port || 1).toString(),
-      PGHOST: dbURL.hostname || '',
-      PGDATABASE: (dbURL.pathname || '').replace('/', ''),
-      PGUSER: (dbURL.auth || defaultDB.auth).split(':')[0],
-      PGPASSWORD: (dbURL.auth || defaultDB.auth).split(':')[1],
-    },
-  );
+  // run backup
+  if (readURL.hostname && readURL.auth && readURL.port && readURL.pathname) {
+    await run(
+      'pg_dump',
+      [
+        '-Fc',
+        '--no-acl',
+        '--no-owner',
+        '-h', readURL.hostname,
+        '-U', readURL.auth.split(':')[0],
+        '-p', readURL.port.toString(),
+        '-f', pgDumpLocation,
+        readURL.pathname.replace('/', ''),
+      ],
+      { PGPASSWORD: readURL.auth.split(':')[1] },
+    );
+  }
 
-  fs.unlinkSync(fname);
+  if (dbURL.auth && dbURL.pathname && dbURL.hostname && dbURL.port) {
+    await run(
+      'pg_restore',
+      [
+        '--verbose',
+        '--clean',
+        '--no-acl',
+        '--no-owner',
+        '-U', dbURL.auth.split(':')[0],
+        '-d', dbURL.pathname.replace('/', ''),
+        '-h', dbURL.hostname,
+        '-p', dbURL.port.toString(),
+        pgDumpLocation,
+      ],
+      { PGPASSWORD: dbURL.auth.split(':')[1] },
+    );
+  }
+
+  deleteBackup();
 };
 
 export default copyTables;
